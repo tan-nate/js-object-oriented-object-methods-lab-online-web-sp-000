acb052832ea33cef1d920432dfab87f494ce1ccc
Nate Tan <nktan93@gmail.com>
2019-08-27 19:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function BoardMember (name, homeState, training) {
+function BoardMember (name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
+  this.veto = function(){
+    console.log("No, I must disagree");
+  }
+  this.approve = function(){
+    console.log("You can do that!");
+  }
+  this.doCharity = function(){
+    console.log("I like to help people.");
+  }
+  this.pressStatement = function(){
+    console.log("You will see great things from Scuber.");
+  }
+  this.sayHi = function(){
+    console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`);
+  }
 }
